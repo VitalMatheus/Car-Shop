03f669ea5ea62d9dd1d393bc3a46c527545248a3
Matheus Vital Diniz de Souza <matheus@localhost.localdomain>
2022-09-15 19:31:38 -0300

    Feat: adiciona teste da camada model para criação de um carro

---
+++ src/tests/Mocks/CarMock.ts
@@ -0,0 +1,10 @@
+import { ICar } from '../../interfaces/ICar';
+
+export const carMock: ICar = {
+  model: "Dodge Charger",
+  year: 1969,
+  color: "Orange",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2
+};
---
+++ src/tests/unit/models/car.test.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import CarModel from '../../../models/modelCars';
+import { Model } from 'mongoose';
+import { carMock } from '../../Mocks/CarMock';
+
+describe('Car Model', () => {
+	const carModel = new CarModel();
+
+	before(() => {
+		sinon.stub(Model, 'create').resolves(carMock);
+	});
+
+  after(() => {
+		sinon.restore();
+	});
+
+	describe('creating a car', () => {
+		it('successfully created', async () => {
+			const newCar = await carModel.create(carMock);
+			expect(newCar).to.be.deep.equal(carMock);
+		});
+	});
+
+	// 	it('_id not found', async () => {
+	// 		try {
+	// 			await carModel.readOne('123ERRADO');
+	// 		} catch (error: any) {
+	// 			expect(error.message).to.be.eq('InvalidMongoId');
+	// 		}
+	// 	});
+	// });
+
+});
